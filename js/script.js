f46beea57e5c1b3f08c67d993c08f83a424e763e
runnersingh <jasmeet@bioweave.in>
2025-03-25 18:53:50 -0700

    Updated js to add dropdown for navigation menu on mobile

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   const header = document.querySelector('.site-header');
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.site-nav');
 
+  // Add "scrolled" class to header on scroll
   window.addEventListener('scroll', function() {
     if (window.scrollY > 50) {
       header.classList.add('scrolled');
     } else {
       header.classList.remove('scrolled');
     }
+  });
+
+  // Toggle mobile nav
+  menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
   });
 });
